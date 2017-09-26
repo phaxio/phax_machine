b37b629bc447d71e847bc2998425737adaebff97
mwmayerle <mwmayerle@gmail.com>
2017-09-26 11:26:59 -0500

    a logged in user can only edit themselves

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+		session.delete(:user_id)
+		@current_user = nil
+    redirect_to "/users"
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include UsersHelper
+  
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
