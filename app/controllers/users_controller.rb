413c095706edf262f7405cdf9d51176d8600cdca
mwmayerle <mwmayerle@gmail.com>
2017-09-26 12:50:21 -0500

    added authorized? method to Users controller that compares current_user method id with the set_user method id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,12 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to "/users", notice: 'Please log out to add a new user.'
+    else
+      @user = User.new
+      render :new
+    end
   end
 
   def show
@@ -23,30 +28,47 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new user_params
-
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to users_path, notice: 'User added successfully.'
+    if logged_in?
+      redirect_to "/users", notice: 'Please log out to add a new user.'
     else
-      render :new
+      @user = User.new user_params
+
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to users_path, notice: 'User added successfully.'
+      else
+        render :new
+      end
     end
   end
 
   def edit
+    if authorized?
+      render :edit
+    else
+      redirect_to "/users", notice: 'You cannot edit other users!'
+    end
   end
 
   def update
-    if @user.update user_params
-      redirect_to users_path, notice: 'User updated successfully.'
+    if authorized?
+      if @user.update user_params
+        redirect_to users_path, notice: 'User updated successfully.'
+      else
+        render :edit
+      end
     else
-      render :edit
+      redirect_to "/users", notice: 'You cannot edit other users!'
     end
   end
 
   def destroy
-    @user.destroy
-    redirect_to users_path, notice: 'User deleted successfully.'
+    if authorized?
+      @user.destroy
+      redirect_to users_path, notice: 'User deleted successfully.'
+    else
+      redirect_to "/users", notice: 'You cannot delete other users!'
+    end
   end
 
   private
@@ -91,5 +113,9 @@ class UsersController < ApplicationController
       search_params[:start] = params[:start].to_i if params[:start]
       search_params[:end] = params[:end].to_i if params[:end]
       search_params
+    end
+
+    def authorized?
+      set_user.id == current_user.id ? true : false
     end
 end
