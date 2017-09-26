837bdd290cf0ce214a07f60d5acf10e18c3abdb6
mwmayerle <mwmayerle@gmail.com>
2017-09-26 11:02:05 -0500

    added users_helper with current_user and logged_in? methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,16 @@
 class SessionsController < ApplicationController
 	def new
+		@user = User.new
 	end
 
 	def create
+		@user = User.find_by(email: params[:login][:email])
+    if @user && @user.authenticate(params[:login][:password])
+      session[:user_id] = @user.id
+      redirect_to '/users'
+    else
+      render :new
+    end
 	end
 
 	def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
-  resources :sessions only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
   get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  post "/logout" => "sessions#destroy"
 
   mount PhaxMachineSinatra => "/"
 end
