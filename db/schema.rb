fd181723dcd68bdf25a80753e0dbfc320dc0cc9d
mwmayerle <mwmayerle@gmail.com>
2017-10-26 11:04:08 -0500

    Tweaked jaavascript to include a variable, updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20171017190006) do
 
   create_table "users", force: :cascade do |t|
     t.string "fax_number", null: false
-    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "fax_tag", null: false
