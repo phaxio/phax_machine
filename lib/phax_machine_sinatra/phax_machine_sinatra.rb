d74e4bdb0bb1daf3d000fcf371496ddb8feffbe5
Matt Mayerle <mwmayerle@gmail.com>
2018-04-27 15:15:57 -0500

    altered fax_received db query

--- lib/phax_machine_sinatra/phax_machine_sinatra.rb
+++ lib/phax_machine_sinatra/phax_machine_sinatra.rb
@@ -114,7 +114,7 @@ class PhaxMachineSinatra < Sinatra::Application
     begin
       user_id = db[:user_fax_numbers].where(fax_number: recipient_number).first[:user_id]
       p user_id
-      email_addresses = db[:user_emails].where(user_id: user_ids).all.map { |user_email| user_email[:email] }
+      email_addresses = db[:user_emails].where(user_id: user_id).all.map { |user_email| user_email[:email] }
       p email_addresses
     ensure
       db.disconnect
