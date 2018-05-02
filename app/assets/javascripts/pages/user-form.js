384f52b918bdd46cc55092c4d1cc408a2ff4d823
Matt Mayerle <mwmayerle@gmail.com>
2018-05-02 18:09:21 -0500

    Added JS functions to declare a primary number

--- app/assets/javascripts/pages/user-form.js
+++ app/assets/javascripts/pages/user-form.js
@@ -1,5 +1,7 @@
 PhaxMachine.pages['user-form'] = {
     render: function() {
+    		ensureNoBlankRadioButtons();
+
         $('#addUserEmail').on('click', function() {
             var emailInputs = $('#userEmailList input[type=email]');
             var nextIdx = emailInputs.length;
@@ -28,13 +30,14 @@ PhaxMachine.pages['user-form'] = {
             newFaxInput.attr('value', '');
             newFaxInput.val('');
 
-            // Super ugly, but this time Matt copy-pasted what's above b/c he doesn't know Vue.js
+            // Super ugly, but this time Matt copy-pasted what's above b/c he doesn't know Vue.js and didn't want to accidentally break anything
             var inputFaxGroup = $('<div class="input-group"></div>')
+            var inputRadioButton = $('<div class="radio"><input class="radio-btn" type="radio" value="false" name="user[user_fax_numbers_attributes][' + nextFaxIdx + '][primary_number]"></div>')
             var hiddenFaxFieldHtml = '<input class="destroy-field" type="hidden" value="false" name="user[user_fax_numbers_attributes][' + nextFaxIdx + '][_destroy]" id="user_user_fax_numbers_attributes_' + nextFaxIdx + '__destroy">';
             var inputFaxGroupBtn = $('<span class="input-group-btn">' + hiddenFaxFieldHtml + '<a class="btn btn-secondary btn-remove-tel" ><i class="glyphicon glyphicon-trash"></i></a></span>');
+            inputFaxGroup.append(inputRadioButton);
             inputFaxGroup.append(newFaxInput);
             inputFaxGroup.append(inputFaxGroupBtn);
-
             $('#userFaxNumberList').append(inputFaxGroup);
         });
 
@@ -51,31 +54,65 @@ PhaxMachine.pages['user-form'] = {
         
         $(document).on('click', '.btn-remove-tel', function() {
             var inputFaxGroup = $(this).closest('.input-group');
-	            inputFaxGroup.find('input[type="tel"]');
-	            inputFaxGroup.find('.destroy-field').val(true);
-	            inputFaxGroup.hide();
+	          inputFaxGroup.find('input[type="tel"]').val('12223334444');
+	          inputFaxGroup.find('.destroy-field').val(true);
+	          inputFaxGroup.find('.radio-btn').prop('value', false);
+        		inputFaxGroup.find('.radio-btn').prop('checked', false);
+	          inputFaxGroup.hide();
+        		ensureNoBlankRadioButtons();
         });
 
         $(document).on('click', '.radio-btn', function() {
+        		ensureNoBlankRadioButtons();
         		var radioButtonValue = $(this).prop('value');
         		var allRadioButtons = $('.radio-btn');
         		if (radioButtonValue) {
         				allRadioButtons.each(function() {
-        						if ($(this).prop('value') && $(this).attr('id') !== $(event.target).attr('id')) {
+        						if ($(this).prop('value') && $(this).attr('id') === $(event.target).attr('id')) {
+        							return;
+        						} else if ($(this).prop('value') && $(this).attr('id') !== $(event.target).attr('id')) {
         								$(this).prop('value', false);
-        								$(this).attr('checked', false);
-        								console.log($(this).prop('checked'))
-        						} else if ($(this).prop('value') && $(this).attr('id') === $(event.target).attr('id')) {
-        								$(this).prop('value', true);
-        								$(this).attr('checked', true);
-        								console.log($(this).prop('checked'))
+        								$(this).prop('checked', false);
         						} else {
         								$(this).prop('value', false);
-        								$(this).attr('checked', false);
-        								console.log($(this).prop('checked'))
+        								$(this).prop('checked', false);
         						}
         				})
         		}
         });
     }
+}
+
+function ensureNoBlankRadioButtons() {
+  var allRadioButtons = $('.radio-btn');
+	if (radioButtonIsChecked(allRadioButtons)) {
+		$('#submit-button').removeClass('disabled');
+		$('#submit-button').prop('disabled', false);
+	} else {
+		$('#submit-button').addClass('disabled');
+		$('#submit-button').prop('disabled', true);
+		createUserAlert("danger", "Please select a primary number before continuing");
+	}
+}
+
+function radioButtonIsChecked(allRadioButtons) {
+	var isChecked = false;
+	allRadioButtons.each(function() {
+		if ($(this).prop('checked') === true) {
+			isChecked = true;
+		}
+	});
+	return isChecked;
+}
+
+function createUserAlert(type, message) {
+	var alert = $("<div class='alert alert-dismissable alert-" + type + "'></div>")
+	var closeAlertButton = $(
+		'<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+		+ '<span aria-hidden="true">&times;</span>'
+		+ '</button>'
+	)
+	alert.append(closeAlertButton)
+	alert.append(message)
+	$("form").prepend(alert)
 }
